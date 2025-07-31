53b4e5238166a91d623d49ddc15c888e68e56507
Nielen Seldte <129792303+nielenseldte@users.noreply.github.com>
2025-07-31 16:34:12 +0200

    implemented the playRound method

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ const computerPlay = () => {
     randomIndex = Math.floor(Math.random() * plays.length);
     return plays[randomIndex];
 }
+
+const playRound = (playerSelection, computerSelection) => {
+    player = String(playerSelection).toLowerCase();
+    computer = String(computerSelection).toLowerCase();
+    
+    if (player == 'rock' && computer == 'scissors') {
+        return "You win!, Rock beats Scissors";
+    } else if (player == 'rock' && computer == 'paper') {
+        return "You Lose! Paper beats Rock";
+    } else if (player == 'scissors' && computer == 'rock') {
+        return "You Lose! Rock beats Scissors";
+    } else if (player == 'scissors' && computer == 'paper') {
+        return "You Win! Scissors beats Paper";
+    } else if (player == 'paper' && computer == 'rock') {
+        return "You Win! Paper beats Rock";
+    } else if (player == 'paper' && computer == 'scissors') {
+        return "You Lose! Scissors beats Paper";
+    } else {
+        return "Draw!";
+    }
+}
