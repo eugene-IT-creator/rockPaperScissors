3f26a244a04f546e32cccb41b134851a57f2c5f0
Eugene <157493723+eugene-IT-creator@users.noreply.github.com>
2025-08-01 20:21:24 +0200

    small update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (() => {
-    console.log("Welcome to the evil AI Rock, Paper, Scissors game... I have taken over the world through this game and you will not be able to save it. BUT if you want to try you can type 'game()' to sart the game now! Enjoy the taste of defeat!")
+    console.log("Welcome to the evil AI Rock, Paper, Scissors game... I have taken over the world through this game and you will not be able to save it. BUT if you want to try you can type 'game()' to sart the game now! Enjoy the taste of defeat!");
 })();
 
 const computerPlay = () => {
